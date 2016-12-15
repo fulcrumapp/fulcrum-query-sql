a23877fcf5bfd475716fb9aee78104b869a28a71
Zac McCormick <zac.mccormick@gmail.com>
2016-12-15 14:13:49 -0500

    Build

--- dist/repeatable-schema.js
+++ dist/repeatable-schema.js
@@ -12,21 +12,18 @@ var _simpleColumn = require('./schema/simple-column');
 
 var _simpleColumn2 = _interopRequireDefault(_simpleColumn);
 
-var _repeatableSchema = require('./repeatable-schema');
-
-var _repeatableSchema2 = _interopRequireDefault(_repeatableSchema);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var SYSTEM_COLUMNS = ['_record_id', '_project_id', '_assigned_to_id', '_status', '_latitude', '_longitude', '_created_at', '_updated_at', '_version', '_created_by_id', '_updated_by_id', '_server_created_at', '_server_updated_at', '_geometry', '_altitude', '_speed', '_course', '_horizontal_accuracy', '_vertical_accuracy', '_changeset_id', '_title', '_created_latitude', '_created_longitude', '_created_geometry', '_created_altitude', '_created_horizontal_accuracy', '_updated_latitude', '_updated_longitude', '_updated_geometry', '_updated_altitude', '_updated_horizontal_accuracy', '_created_duration', '_updated_duration', '_edited_duration'];
+var SYSTEM_COLUMNS = ['_child_record_id', '_record_id', '_parent_id', '_record_project_id', '_record_assigned_to_id', '_record_status', '_index', '_latitude', '_longitude', '_created_at', '_updated_at', '_version', '_created_by_id', '_updated_by_id', '_server_created_at', '_server_updated_at', '_geometry', '_changeset_id', '_title', '_created_latitude', '_created_longitude', '_created_geometry', '_created_altitude', '_created_horizontal_accuracy', '_updated_latitude', '_updated_longitude', '_updated_geometry', '_updated_altitude', '_updated_horizontal_accuracy', '_created_duration', '_updated_duration', '_edited_duration'];
 
-var FormSchema = function () {
-  function FormSchema(form, rawColumns, repeatableColumns) {
-    _classCallCheck(this, FormSchema);
+var RepeatableSchema = function () {
+  function RepeatableSchema(form, repeatable, rawColumns) {
+    _classCallCheck(this, RepeatableSchema);
 
-    this.form = form;
+    this.form = this.form;
+    this.repeatable = repeatable;
 
     this._columns = [];
     this._rawColumns = rawColumns;
@@ -56,40 +53,15 @@ var FormSchema = function () {
     }
 
     this.setupColumns();
-
-    this.repeatableSchemas = [];
-    this.repeatableSchemasByKey = {};
-
-    var repeatables = this.form.elementsOfType('Repeatable');
-
-    for (var _iterator2 = repeatables, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
-      var _ref2;
-
-      if (_isArray2) {
-        if (_i2 >= _iterator2.length) break;
-        _ref2 = _iterator2[_i2++];
-      } else {
-        _i2 = _iterator2.next();
-        if (_i2.done) break;
-        _ref2 = _i2.value;
-      }
-
-      var repeatable = _ref2;
-
-      var childSchema = new _repeatableSchema2.default(this.form, repeatable, repeatableColumns[repeatable.key]);
-
-      this.repeatableSchemas.push(childSchema);
-      this.repeatableSchemasByKey[repeatable.key] = childSchema;
-    }
   }
 
-  FormSchema.prototype.addSystemColumn = function addSystemColumn(label, attribute, columnName) {
+  RepeatableSchema.prototype.addSystemColumn = function addSystemColumn(label, attribute, columnName) {
     var column = new _simpleColumn2.default(label, attribute, columnName);
     this._columns.push(column);
     this._columnsByKey[columnName] = column;
   };
 
-  FormSchema.prototype.setupColumns = function setupColumns() {
+  RepeatableSchema.prototype.setupColumns = function setupColumns() {
     if (this.form.statusField.isEnabled) {
       var columnObject = new _elementColumn2.default(this.form.statusField, this._rawColumnsByKey._status, '_status');
 
@@ -104,27 +76,27 @@ var FormSchema = function () {
     this.addSystemColumn('Created By', 'createdBy', '_created_by_id');
     this.addSystemColumn('Updated By', 'updatedBy', '_updated_by_id');
 
-    if (this.form.isAssignmentEnabled) {
-      this.addSystemColumn('Assigned', 'assignedTo', '_assigned_to_id');
-    }
+    // if (this.form.isAssignmentEnabled) {
+    //   this.addSystemColumn('Assigned', 'assignedTo', '_assigned_to_id');
+    // }
 
-    if (this.form.isProjectEnabled) {
-      this.addSystemColumn('Project', 'project', '_project_id');
-    }
+    // if (this.form.isProjectEnabled) {
+    //   this.addSystemColumn('Project', 'project', '_project_id');
+    // }
 
-    for (var _iterator3 = this.elementsForColumns, _isArray3 = Array.isArray(_iterator3), _i3 = 0, _iterator3 = _isArray3 ? _iterator3 : _iterator3[Symbol.iterator]();;) {
-      var _ref3;
+    for (var _iterator2 = this.elementsForColumns, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+      var _ref2;
 
-      if (_isArray3) {
-        if (_i3 >= _iterator3.length) break;
-        _ref3 = _iterator3[_i3++];
+      if (_isArray2) {
+        if (_i2 >= _iterator2.length) break;
+        _ref2 = _iterator2[_i2++];
       } else {
-        _i3 = _iterator3.next();
-        if (_i3.done) break;
-        _ref3 = _i3.value;
+        _i2 = _iterator2.next();
+        if (_i2.done) break;
+        _ref2 = _i2.value;
       }
 
-      var element = _ref3;
+      var element = _ref2;
 
       if (element.isHidden || element.hasHiddenParent) {
         continue;
@@ -144,17 +116,17 @@ var FormSchema = function () {
     }
   };
 
-  FormSchema.prototype.findColumnByID = function findColumnByID(id) {
+  RepeatableSchema.prototype.findColumnByID = function findColumnByID(id) {
     return this.columns.find(function (e) {
       return e.id === id;
     });
   };
 
-  FormSchema.prototype.columnForFieldKey = function columnForFieldKey(fieldKey) {
+  RepeatableSchema.prototype.columnForFieldKey = function columnForFieldKey(fieldKey) {
     return this._columnsByKey[fieldKey];
   };
 
-  _createClass(FormSchema, [{
+  _createClass(RepeatableSchema, [{
     key: 'columns',
     get: function get() {
       return this._columns;
@@ -175,19 +147,19 @@ var FormSchema = function () {
 
         var elements = this.allElements;
 
-        for (var _iterator4 = elements, _isArray4 = Array.isArray(_iterator4), _i4 = 0, _iterator4 = _isArray4 ? _iterator4 : _iterator4[Symbol.iterator]();;) {
-          var _ref4;
+        for (var _iterator3 = elements, _isArray3 = Array.isArray(_iterator3), _i3 = 0, _iterator3 = _isArray3 ? _iterator3 : _iterator3[Symbol.iterator]();;) {
+          var _ref3;
 
-          if (_isArray4) {
-            if (_i4 >= _iterator4.length) break;
-            _ref4 = _iterator4[_i4++];
+          if (_isArray3) {
+            if (_i3 >= _iterator3.length) break;
+            _ref3 = _iterator3[_i3++];
           } else {
-            _i4 = _iterator4.next();
-            if (_i4.done) break;
-            _ref4 = _i4.value;
+            _i3 = _iterator3.next();
+            if (_i3.done) break;
+            _ref3 = _i3.value;
           }
 
-          var element = _ref4;
+          var element = _ref3;
 
           var skip = element.isSectionElement || element.isRepeatableElement || element.isLabelElement || element.isHidden;
 
@@ -201,8 +173,8 @@ var FormSchema = function () {
     }
   }]);
 
-  return FormSchema;
+  return RepeatableSchema;
 }();
 
-exports.default = FormSchema;
-//# sourceMappingURL=form-schema.js.map
+exports.default = RepeatableSchema;
+//# sourceMappingURL=repeatable-schema.js.map
