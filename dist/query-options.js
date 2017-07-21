4fe6378e2391c51c71d67acdfb7e39083b6a8f2c
Zac McCormick <zac.mccormick@gmail.com>
2017-07-21 16:04:12 -0400

    Simplify

--- dist/query-options.js
+++ dist/query-options.js
@@ -2,8 +2,6 @@
 
 exports.__esModule = true;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var QueryOptions = function () {
@@ -12,7 +10,7 @@ var QueryOptions = function () {
 
     _classCallCheck(this, QueryOptions);
 
-    this._timeZone = attrs.time_zone || null;
+    this.timeZone = attrs.time_zone || null;
   }
 
   QueryOptions.prototype.toJSON = function toJSON() {
@@ -20,16 +18,6 @@ var QueryOptions = function () {
       time_zone: this.timeZone
     };
   };
-
-  _createClass(QueryOptions, [{
-    key: "timeZone",
-    get: function get() {
-      return this._timeZone;
-    },
-    set: function set(timeZone) {
-      this._timeZone = timeZone;
-    }
-  }]);
 
   return QueryOptions;
 }();
