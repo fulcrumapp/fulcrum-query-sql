756448e01efb57c0c4227ace8a0371518e197e8b
Juan Acosta <juan.acosta@spatialnetworks.com>
2022-06-22 19:16:11 -0500

    Add System columns

--- src/form-schema.js
+++ src/form-schema.js
@@ -152,6 +152,8 @@ export default class FormSchema extends FormFieldSchema {
       this.addSystemColumn('Created Duration', 'createdDuration', '_created_duration', 'integer');
       this.addSystemColumn('Updated Duration', 'updatedDuration', '_updated_duration', 'integer');
       this.addSystemColumn('Edited Duration', 'editedDuration', '_edited_duration', 'integer');
+      this.addSystemColumn('Record Key', 'recordKey', '_record_key', 'string');
+      this.addSystemColumn('Record Sequence', 'recordSequence', '_record_sequence', 'integer');
     }
 
     this.setupElementColumns();
