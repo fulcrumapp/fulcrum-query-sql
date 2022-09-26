ae10dfec8600a3ae8efc5cb404ecf0efc10ed833
Miguel Malcolm <miguel.malcolm@spatialnetworks.com>
2022-09-26 15:45:03 -0400

    try logging

--- src/query.js
+++ src/query.js
@@ -351,7 +351,6 @@ export default class Query {
       }
     }
 
-    console.log('schema: ', this.schema);
     if (this.schema.createdByColumn) {
       if (subJoinColumns.indexOf(this.schema.createdByColumn) === -1) {
         joinedColumns.push(ResTarget(ColumnRef('name', 'created_by'), 'created_by'));
