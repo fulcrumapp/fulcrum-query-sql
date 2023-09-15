c3a50d9d0445bf4ef19c28d3c3e216d32715d191
dotconde <iamdeyvi@gmail.com>
2023-09-15 10:42:22 -0500

    remove changes from calculateDateRange

--- src/operator.js
+++ src/operator.js
@@ -621,17 +621,13 @@ export function availableOperatorsForColumn(column) {
   return operators;
 }
 
-export function calculateDateRange(column, operator, value, now) {
+export function calculateDateRange(operator, value, now) {
   now = moment(now || new Date()).clone().startOf('day');
 
   const date1 = now.clone();
   const date2 = now.clone();
 
   const range = (start, end) => {
-    if (column.type === 'date') {
-      return [ start.clone(), (end || start).clone() ];
-    }
-
     return [ start.clone(), (end || start).clone().endOf('day') ];
   };
 
