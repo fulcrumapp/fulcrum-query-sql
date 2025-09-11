8f666f2427077485aceafc678d42ab3bc9951641
Em Jirles <emily.jirles@fulcrumapp.com>
2025-09-11 16:31:43 -0400

    Try some console logs

--- src/ast/helpers.js
+++ src/ast/helpers.js
@@ -18,7 +18,9 @@ export function StringValue(value) {
   let str = value != null ? value.toString() : null;
   const isUUID = str?.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i);
   if (!isUUID && !isNaN(Number(str[0]))) {
+    console.log('not a UUID and starts with number', str);
     str = `\"${str}\"`;
+    console.log('converted to', str);
   }
   return {
     String: {
